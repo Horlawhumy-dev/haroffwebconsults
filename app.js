1039f937d52ec4ffc82fac1a18979e1a9cd65cb3
Arafat Olayiwola <harof.dev@gmail.com>
2021-02-14 18:46:55 +0100

    modified some files

--- app.js
+++ app.js
@@ -19,3 +19,24 @@ closeHamburger.addEventListener("click", () => {
     // console.log(ulLinks) 
     ulLinks.classList.toggle('toggle-close');
 })
+
+// Scrolling Top Button
+const scrollBtn = document.getElementById('scroll-top');
+
+scrollBtn.addEventListener('click', (e) => {
+    // document.documentElement.scrollTop = '0';
+    currentYOffset = self.pageYOffset;
+    initYOffset = currentYOffset;
+
+    var intervalId = setInterval(function(){
+    currentYOffset -= initYOffset*0.05; 
+    document.body.scrollTop = currentYOffset ;
+    document.documentElement.scrollTop = currentYOffset;
+
+        if(self.pageYOffset == 0){
+        clearInterval(intervalId);
+        }
+    }, 15);
+
+    e.preventDefault();
+})
